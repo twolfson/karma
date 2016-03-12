6b7e547026fcb2de7c554e901fe22c14a43eb36c
Todd Wolfson <todd@twolfson.com>
2016-03-11 23:13:27 -0600

    Built a bridge to parent window

--- context/main.js
+++ context/main.js
@@ -2,11 +2,35 @@
 // sets window.__karma__ and overrides console and error handling
 // Use window.opener if this was opened by someone else - in a new window
 (function () {
-  window.addEventListener('message', function handleMessage (evt) {
-    console.log('child got message');
-  });
+
+  // Call our initialization function
   var parentWindow = window.opener || window.parent;
   parentWindow.karma.setupContext(window);
-  console.log('postMessage', !!parentWindow.postMessage);
-  window.parent.postMessage('hi', window.location.origin);
+
+  // Define a remote call method for Karma
+  window.callParentKarmaMethod = function (method, args) {
+    // If the method doesn't exist, then error out
+    if (!parentWindow.karma[method]) {
+      parentWindow.karma.error('Expected Karma method "' + method + '" to exist but it doesn\'t');
+      return;
+    }
+
+    // Otherwise, run our method
+    parentWindow.karma[method].apply(parentWindow.karma, args);
+  };
+
+  // If we don't have access to the window, then use `postMessage`
+  // DEV: In Electron, we don't have access to the parent window due to it being in a separate process
+  // DEV: We avoid using this in Internet Explorer as they only support strings
+  //   http://caniuse.com/#search=postmessage
+  var haveParentAccess = false;
+  try { haveParentAccess = !!parentWindow.window; } catch (err) { /* Ignore errors (likely permisison errors) */ }
+  if (!haveParentAccess) {
+    callParentKarma = function (method, args) {
+      parentWindow.postMessage({method: method, arguments: args}, window.location.origin);
+    };
+  }
+
+  // Run an example method
+  window.callParentKarmaMethod('log', ['log', ['hi']]);
 }());
