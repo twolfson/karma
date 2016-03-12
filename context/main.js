6cd76478e63dad061dd69e8429e0d8135cf57455
Todd Wolfson <todd@twolfson.com>
2016-03-11 23:18:10 -0600

    Cleaned up postMessage work from earlier

--- context/main.js
+++ context/main.js
@@ -2,7 +2,6 @@
 // sets window.__karma__ and overrides console and error handling
 // Use window.opener if this was opened by someone else - in a new window
 (function () {
-
   // Call our initialization function
   var parentWindow = window.opener || window.parent;
   parentWindow.karma.setupContext(window);
